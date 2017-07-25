46815d6a9f949d945778f3dba1e43c45ea71e796
Jon Schneider <jkschneider@gmail.com>
2017-07-25 13:56:40 -0500

    Add Google analytics, add common tags docs

--- src/index.js
+++ src/index.js
@@ -3,9 +3,17 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom'
 import App from './components/App/App';
 import registerServiceWorker from './registerServiceWorker';
+import { createBrowserHistory } from 'history';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css'; // where we wish to override bootstrap defaults
 
-ReactDOM.render(<BrowserRouter><App/></BrowserRouter>, document.getElementById('root'));
+const history = createBrowserHistory();
+
+history.listen((location) => {
+  window.ga('set', 'page', location.pathname + location.search);
+  window.ga('send', 'pageview');
+});
+
+ReactDOM.render(<BrowserRouter history={history}><App/></BrowserRouter>, document.getElementById('root'));
 registerServiceWorker();
