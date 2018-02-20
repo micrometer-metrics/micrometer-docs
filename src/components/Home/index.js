a76ddf24569be68ecd37c9291224d0551d4bdc10
Jon Schneider <jschneider@pivotal.io>
2018-02-20 17:15:09 -0600

    Add filters doc, fix list of supported registries on landing page

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -13,7 +13,8 @@ export default function Home() {
           </h1>
           <p className="lead" style={{ padding: 8, color: 'white', background: 'rgba(52, 48, 45, 0.8)' }}>Micrometer
             provides a simple facade over the instrumentation clients for the most popular monitoring systems,
-            allowing you to instrument your JVM-based application code without vendor lock-in. Think SLF4J, but for metrics.
+            allowing you to instrument your JVM-based application code without vendor lock-in. Think SLF4J, but for
+            metrics.
           </p>
         </div>
       </div>
@@ -22,7 +23,9 @@ export default function Home() {
           <div className="col-lg-4 text-center">
             <i className="fa fa-4x fa-database" aria-hidden="true" />
             <h2>Dimensional Metrics</h2>
-            <p>Micrometer provides vendor-neutral interfaces for <strong>timers</strong>, <strong>gauges</strong>, <strong>counters</strong>, <strong>distribution summaries</strong>, and <strong>long task timers</strong> with a dimensional data model that, when paired with a dimensional monitoring system, allows for efficient
+            <p>Micrometer provides vendor-neutral interfaces for <strong>timers</strong>, <strong>gauges</strong>,
+              <strong>counters</strong>, <strong>distribution summaries</strong>, and <strong>long task timers</strong>
+              with a dimensional data model that, when paired with a dimensional monitoring system, allows for efficient
               access to a particular named metric with the ability to drill down across its dimensions.</p>
           </div>
           <div className="col-lg-4 text-center">
@@ -43,13 +46,15 @@ export default function Home() {
         <div className="row justify-content-center" style={{ padding: 30 }}>
           <div className="col-lg-6 col-md-12">
             <h2>Support for popular monitoring systems</h2>
-            <p>As an instrumentation facade, Micrometer allows you to instrument your code with dimensional metrics with a
-              vendor-neutral interface and decide on the monitoring system as a last step. Instrumenting your core library
+            <p>As an instrumentation facade, Micrometer allows you to instrument your code with dimensional metrics with
+              a
+              vendor-neutral interface and decide on the monitoring system as a last step. Instrumenting your core
+              library
               code with Micrometer allows the libraries to be included in applications that ship metrics to different
               backends.</p>
-          <p>
-              Contains built-in support for <strong>Prometheus</strong>, Netflix <strong>Atlas</strong>,
-              and <strong>Datadog</strong>. Support for InfluxDB, statsd, and Graphite are coming.</p>
+            <p>
+              Contains built-in support for <strong>Prometheus</strong>, Netflix <strong>Atlas</strong>, <strong>CloudWatch</strong>, <strong>Datadog</strong>, <strong>Graphite</strong>, <strong>Ganglia</strong>, <strong>JMX</strong>, <strong>Influx/Telegraf</strong>, <strong>StatsD</strong>, <strong>SignalFx</strong>, and <strong>Wavefront</strong>.
+            </p>
           </div>
         </div>
       </div>
