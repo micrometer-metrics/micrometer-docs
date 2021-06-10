312b772fa602fe774f8fee8ef83e78da30e1a541
Tommy Ludwig <8924140+shakuzen@users.noreply.github.com>
2021-06-10 12:33:21 +0900

    Make Spring Boot 1.5.x EOL more prominent
    Remove mention of it from the installing documentation.
    List it second in the index with an EOL moniker.

--- src/components/DocRoot/index.js
+++ src/components/DocRoot/index.js
@@ -134,14 +134,14 @@ export default function DocRoot() {
         <li><span className="doc-section">Reference</span>. Detailed list of out-of-the-box
           instrumentation provided by Micrometer.
           <ul>
-            <li><span className="doc-section">Spring Boot</span>. As of Spring Boot 2.0.0.M5, Micrometer is the
+            <li><span className="doc-section">Spring Boot</span>. As of Spring Boot 2.0, Micrometer is the
               instrumentation library powering the delivery of application metrics from Spring. The <i>micrometer-spring-legacy </i>
                module provides drop-down support for Spring Boot 1.5.x.
               <ul>
-                <li><NavLink className="doc-section" to="/docs/ref/spring/1.5">Spring Boot 1.5.x</NavLink></li>
                 <li><a className="doc-section"
                        href="https://docs.spring.io/spring-boot/docs/current/reference/htmlsingle/#production-ready-metrics">Spring
-                  Boot 2.0</a></li>
+                  Boot 2.x</a></li>
+                <li><NavLink className="doc-section" to="/docs/ref/spring/1.5">Spring Boot 1.5.x</NavLink> (no longer supported)</li>
               </ul>
             </li>
             <li><NavLink className="doc-section" to="/docs/ref/jvm">JVM</NavLink>. Metrics on classloaders, memory,
